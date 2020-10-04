855e70624eea066bb01751761915ae013223d52d
NavidFayezi <navidfayezi.98@gmail.com>
2020-10-04 19:55:09 +0330

    initial commit

---
+++ hw1.c
@@ -0,0 +1,17 @@
+/*
+ * hw1.c
+ *
+ * Created: 04/10/2020 07:34:08 È.Ù
+ * Author: Markazi.co
+ */
+
+#include <io.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
